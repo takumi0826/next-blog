e372f8eedb1b060c60aff8998c26b5555cc2a439
齋藤拓海 <takumi.s8.26@gmail.com>
2021-08-09 01:50:47 +0900

    調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import 'assets/styles/globals.css'
 import { Layout } from 'components/layouts'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { Header, Footer } from 'components/layouts'
 import { GA_TRACKING_ID, pageview } from 'lib/gtag'
 import { AppProps } from 'next/app'
@@ -10,7 +10,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   const container = {
     hidden: { y: 0 },
     show: {
-      y: '-100vh',
+      y: '-100%',
       transition: {
         ease: [1, 0.01, 0.49, 1.05],
         duration: 0.6,
@@ -37,6 +37,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
             className="w-[100vw] min-h-[100%] from-blue-600 via-teal-500 to-purple-500 bg-gradient-to-tl fixed left-0 top-0 z-40"
             initial="hidden"
             animate="show"
+            exit="exit"
             variants={container}
           ></motion.div>
           <motion.div
@@ -44,9 +45,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.5 }}
           >
-            <AnimatePresence exitBeforeEnter>
-              <Component {...pageProps} />
-            </AnimatePresence>
+            <Component {...pageProps} />
           </motion.div>
           <Footer />
         </>
@@ -55,17 +54,16 @@ const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
     default: {
       return (
         <>
-          <motion.div
-            key={router.route}
-            className="w-[100vw] min-h-[100%] from-blue-600 via-teal-500 to-purple-500 bg-gradient-to-tl fixed left-0 top-0 z-40"
-            initial="hidden"
-            animate="show"
-            variants={container}
-          ></motion.div>
           <Layout>
-            <AnimatePresence exitBeforeEnter>
-              <Component {...pageProps} />
-            </AnimatePresence>
+            <motion.div
+              key={router.route}
+              className="w-[100vw] min-h-[100%] from-blue-600 via-teal-500 to-purple-500 bg-gradient-to-tl fixed left-0 top-0 z-40"
+              initial="hidden"
+              animate="show"
+              exit="exit"
+              variants={container}
+            ></motion.div>
+            <Component {...pageProps} />
           </Layout>
         </>
       )
